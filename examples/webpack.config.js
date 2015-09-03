92acdce3784e317cf6ad433a72b29c9c0753f812
Peter Kaleta <mail@peterkaleta.com>
2015-09-03 23:07:39 +0200

    examples dir

---
+++ examples/webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpackConf = require('peters-toolbelt').webpack;
+
+var plugins = [];
+
+plugins.push(new HtmlWebpackPlugin({
+  filename: 'index.html',
+  template: './examples/index.template.html',
+}));
+
+
+var conf = new webpackConf({
+                entry: './index.jsx',
+                output: {
+                    path: path.join(__dirname, '/examples/build'),
+                    filename: 'index.js'
+                },
+                plugins: plugins
+            })
+            .iNeedReact()
+            .iNeedHotDevServer()
+            .getConfig();
+
+module.exports = conf;
