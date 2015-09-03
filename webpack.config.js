11d63e709bef5534a7e24bb27b9d321dc72b383b
Peter Kaleta <mail@peterkaleta.com>
2015-09-03 23:33:27 +0200

    make webpack conf compliant with new toolbelt

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,10 @@ var path = require('path');
 var webpackConf = require('peters-toolbelt').webpack;
 var plugins = [];
 var conf = new webpackConf({
-                entry: './client',
+                entry: './src',
                 output: {
                     path: path.join(__dirname, '/dist'),
                     filename: 'index.js'
-                },
-                resolve: {
-                   alias: {
-                       'react': __dirname + '/node_modules/react',
-                       'react/addons': __dirname + '/node_modules/react/addons',
-                   },
                 },
                 plugins: plugins
             })
