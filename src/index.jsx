016b932b55f9d135b7c3678ff982eb02bec2e864
Peter Kaleta <mail@peterkaleta.com>
2015-09-03 23:32:50 +0200

    basic class def

---
+++ src/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import radium from 'radium';
+
+const styles = {
+  wrapper: {
+  },
+};
+
+@radium
+export default class TokenAutocomplete extends React.Component{
+
+  static displayName = 'TokenAutocomplete';
+
+  static propTypes = {
+  }
+
+  contextTypes = {
+  }
+
+  static defaultProps = {
+  }
+
+  state = {
+  }
+
+  render() {
+    return (
+      <div ref="wrapper" style={styles.wrapper}>
+      </div>
+    );
+  }
+}
