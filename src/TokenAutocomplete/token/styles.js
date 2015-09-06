ca02a604d323bd61fde5487b1b422d94c1a6cbca
Peter Kaleta <mail@peterkaleta.com>
2015-09-06 18:03:28 +0200

    token remove btn styling

--- src/TokenAutocomplete/token/styles.js
+++ src/TokenAutocomplete/token/styles.js
@@ -5,24 +5,32 @@ export default {
     background: '#aaa',
     marginRight: 3,
     padding: 0,
-    border: '1px solid #888'
+    overflow: 'hidden',
+    position: 'relative',
+    border: '1px solid #999'
   },
   removeBtn: {
     display: 'inline-block',
     marginLeft: 2,
-    padding: '2px 6px 2px 6px',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    padding: '4px 6px',
     color: '#888',
     fontSize: 10,
+    borderLeft: '1px solid #888',
     lineHeight: '100%',
+    position: 'absolute',
     verticalAlign: 'middle',
     cursor: 'pointer',
     ':hover': {
-      color: '#666'
+      color: '#777',
+      background: '#999'
     }
   },
   value: {
-    borderRight: '1px solid #888',
     padding: '2px 5px',
+    marginRight: 20,
     display: 'inline-block',
     verticalAlign: 'middle'
   }
