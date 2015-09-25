c3df576ef6a074f1e6e1c5ca24bacc34a2650dec
Peter Kaleta <mail@peterkaleta.com>
2015-09-24 23:00:26 -0400

    new gh generator

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -4,5 +4,7 @@ git checkout gh-pages
 branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 export BRANCH=$branch
 make build
+git add -A
+git commit -m "new docs"
 git push origin gh-pages
 git checkout master
